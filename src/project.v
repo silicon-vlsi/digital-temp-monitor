9c1d8d3edef506bedeadea9ec9cf6889961c1950
VLSI Lab <vlsi@silicon.ac.in>
2025-04-26 00:26:08 +0530

    update

--- src/project.v
+++ src/project.v
@@ -4,6 +4,15 @@
  */
 //`timescale 1ns / 1ps
 //Put your DEFINES here
+`define RST_COUNT	5'd0
+`define MAX_COUNT	5'd28
+`define CS_LOW_COUNT    5'd4
+`define CS_HIGH_COUNT   5'd20
+`define SPI_LATCH_COUNT 5'd22
+//
+`define SPI_IDLE	2'b00
+`define SPI_READ	2'b01
+`define SPI_LATCH	2'b10
 
 // DO NOT CHANGE THIS MODULE
 module digital_temp_monitor_top (
@@ -24,6 +33,15 @@ module digital_temp_monitor_top (
   assign uio_out[2] = 1'b0;
 //---------------------------------------
 
+// Internal signals 
+reg [4:0] count;
+reg [1:0] spi_state;
+wire CS; 
+reg SCK;
+
+assign uio_out[0] = CS;              //CS-->chip select for LM70
+assign uio_out[1] = SCK;             //SCK--> clock for LM70
+
 //--------------Declaration of intenal signal to ports--------------
 // Although the inputs can directly be used in the code,
 // always good to internally assign them to a more readable port.  
@@ -32,5 +50,46 @@ module digital_temp_monitor_top (
   
 
 // IMPLEMENT CODE HERE
+//SPI CLOCK SCK generator
+ 
+  always @(negedge clk or negedge rst_n)
+  if (~rst_n)
+    SCK <= 1'b0;
+  else if(CS)
+    SCK <= 1'b0;
+  else
+    SCK <= ~SCK;
+
+// Create CS 
+assign CS = ~(spi_state == `SPI_READ);
+
+//State machine to create IDLE,READ,LATCH states ot generate CS, SCK 
+always @(posedge clk or negedge rst_n)
+  if (~rst_n)
+      begin	    
+        spi_state <= `SPI_IDLE;
+      end
+  else if ((count >= `CS_LOW_COUNT) && (count < `CS_HIGH_COUNT) )
+      begin
+        spi_state <= `SPI_READ;
+      end
+  else if (count == `SPI_LATCH_COUNT)
+      begin	    
+        spi_state <= `SPI_LATCH;
+      end
+  else 
+      begin	    
+        spi_state <= `SPI_IDLE;
+      end
+
+
+//5-b counter 
+ always @(posedge clk or negedge rst_n)
+   if (~rst_n)
+       count <= `RST_COUNT;
+   else if (count == `MAX_COUNT)
+       count <= `RST_COUNT;
+   else
+       count <= count + 1'b1;
 
  endmodule
--- test/tb.v
+++ test/tb.v
@@ -46,12 +46,12 @@ initial begin
   $dumpfile("tb.vcd");
   $dumpvars(0, tb);
   rst_n = 1'b0;
-  TEMP_SET = 16'h0033;
+  TEMP_SET = 16'h0C00;
   clk = 1'b1;
   ena = 1'b1;
   #10;
     rst_n = 1'b1;
-  #150;
+  #750;
   $finish(2);   
 end
 
